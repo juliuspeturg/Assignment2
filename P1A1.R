c8c90fee512c7060a49ad6628fe087069e694dab
juliuspeturg <juliusg15@ru.is>
2016-08-25 10:28:29 +0000

    Update P1A1.R

--- P1A1.R
+++ P1A1.R
@@ -16,7 +16,7 @@ fuel.ts = ts(data = fuel$fpi, start = c(1979,1),frequency = 12)
 max_row_training = max(which(fuel$year==2003))
 
 fuel_training = fuel[1:max_row_training,]
-fuel_test = fuel[(max_row_training+1):nrow(fuel)]
+fuel_test = fuel[(max_row_training+1):nrow(fuel),]
 
 #generate time sequence for google plot 
 seq(as.Date("1979/1/1"), as.Date("2004/12/1"), "month")
