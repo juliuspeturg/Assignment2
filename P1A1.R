f2f566cc7a1d5831a66b554b91e7f7fa52912727
juliuspeturg <juliusg15@ru.is>
2016-08-25 13:55:38 +0000

    Update P1A1.R

--- P1A1.R
+++ P1A1.R
@@ -13,7 +13,7 @@ fuel_training = fuel[1:max_row_training,]
 fuel_test = fuel[((max_row_training+1):nrow(fuel)),]
 
 X <- matrix(c(rep(1,nrow(fuel_training)),fuel_training$rtime),nrow=nrow(fuel_training))
-X2 <- matrix(c(rep(1,nrow(fuel_test)),fuel_test$rtime),nrow=nrow(fuel_test))
+X2 <- matrix(c(rep(1,nrow(fuel)),fuel$rtime),nrow=nrow(fuel))
 
 theta <- solve(t(X)%*%X)%*%t(X)%*%fuel_training$fpi
 
@@ -23,19 +23,10 @@ cat(yhat)
 #generate time sequence for google plot 
 date_sequence = seq(as.Date("1979/1/1"), as.Date("2004/12/1"), "month")
 fuel$DATE <- date_sequence
-fuel$yhat <- NA
-fuel$index <- NA
-fuel$index[(1:max_row_training)] <- 'y'
-fuel$index[(max_row_training+1):nrow(fuel)] <- 'yhat'
-fuel$yhat[(1:max_row_training)] <-fuel$fpi[(1:max_row_training)]
-fuel$yhat[(max_row_training+1):nrow(fuel)] <- yhat
+fuel[(nrow(fuel)+1):(2*nrow(fuel)),] <- NA
+fuel$DATE[(nrow(fuel)+1):(2*nrow(fuel))] <- date_sequence
 
 
-#tstplot
-require(ggplot2)
-p <- ggplot(data=fuel, aes(x=rtime, y=fpi, group=1)) + 
-  geom_line() + xlab("Year") + ylab("Fuel Price Index") + 
-  ggtitle("US Fuel Price Index: 1979-2004")
   
   
 plot(p)
