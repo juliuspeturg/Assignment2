71eccd550c6490e64da7b9c6d142903cf14802ff
juliuspeturg <juliusg15@ru.is>
2016-08-25 13:31:20 +0000

    Update P1A1.R

--- P1A1.R
+++ P1A1.R
@@ -24,6 +24,7 @@ cat(yhat)
 date_sequence = seq(as.Date("1979/1/1"), as.Date("2004/12/1"), "month")
 fuel$DATE <- date_sequence
 fuel$yhat <- NA
+fuel$yhat[(1:max_row_training)] <-fuel$fpi[(1:max_row_training)]
 fuel$yhat[(max_row_training+1):nrow(fuel)] <- yhat
 
 
