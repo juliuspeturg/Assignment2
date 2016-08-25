9fcc4ef43e981d68bb673ff06e91c7076cca2002
juliuspeturg <juliusg15@ru.is>
2016-08-25 09:46:46 +0000

    Add files via upload

---
+++ P1A1.R
@@ -0,0 +1,36 @@
+#fjarlægja breytur úr global umhverfi
+rm(list=ls())
+#Setja working directory
+setwd('C:/Users/Julius/Desktop/Skóli Haust 2016/tima/Rscripts/Assignment1')
+#loda library´s
+require(ggplot2)
+require()
+
+#import Data
+fuel = read.csv("fuel.csv", stringsAsFactors = FALSE)
+fuel.ts = ts(data = fuel$fpi, start = c(1979,1),frequency = 12)
+#finna hæsta gildi í fuel sem er með árið 2013, það er december 2013
+max_row_training = max(which(fuel$year==2003))
+
+fuel_training = fuel[1:numrow,]
+fuel_test = fuel[numrow+1:nrow(fuel)]
+
+#generate time sequence for google plot 
+seq(as.Date("1979/1/1"), as.Date("2004/12/1"), "month")
+
+
+
+
+
+
+#tstplot
+ggplot(data=fuel, aes(x=rtime, y=fpi, group=1)) + 
+  geom_point() + xlab("Year") + ylab("Fuel Price Index") + 
+  ggtitle("US Fuel Price Index: 1979-2004")
+
+
+
+
+
+#siggatest
+
