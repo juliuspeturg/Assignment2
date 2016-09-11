b6aa02bcfb25c84e39deca9c97e237822ee9c289
sigurdurjonsson <sigurdurjon15@ru.is>
2016-09-11 12:13:31 +0000

    Update P1A2_SJ.R

--- P1A2_SJ.R
+++ P1A2_SJ.R
@@ -1,7 +1,6 @@
 # Importing data:
 
 library(RCurl)
-library(forecast)
 x = getURL("https://raw.githubusercontent.com/juliuspeturg/Assignment1/master/simdata.csv")
 simdata = read.csv(text=x,stringsAsFactors = FALSE,header=TRUE, sep="")
 
@@ -33,17 +32,26 @@ pacf(y2)
 acf(y3)
 pacf(y3)
 
-arima(y1)
+
+
 
 # A2-Q3: Identify the models using arima
 
+arima(y1)
+auto.arima(y1)
+
+
+
+
+
+require(forecast)
 fit1.arima <- arima(y1, order=c(0,1,0))
 summary(fit1.arima)
 plot(forecast(fit1.arima,12))
 
 # A2-Q4: Simulate a model
 
-ts.sim <- arima.sim(list(order = c(1,0,0), ar = 0.9), n = 250) # líka hægt að nota aðferð í A2-Q7, skoða betur hvort er betra
+ts.sim <- arima.sim(list(order = c(1,0,0), ar = 0.9), n = 250) # ath
 
 
 # A2-Q5: Plot timeseries, ACF and PACF
@@ -67,7 +75,7 @@ acf.plot(ts.sim)
 
 # A2-Q7: Simulate the following series and plot with the plot function (n=250)
 
-ts.sim1 <- arima.sim(n=250, list(ar=c(0.5)))          # ATH hvort þetta er rétt allt saman
+ts.sim1 <- arima.sim(n=250, list(ar=c(0.5)))        
 ts.sim2 <- arima.sim(n=250, list(ar=c(-0.5)))
 ts.sim3 <- arima.sim(n=250, list(ar=c(0.5,0.2)))
 ts.sim4 <- arima.sim(n=250, list(ar=c(0.7)))
@@ -81,9 +89,10 @@ acf.plot(ts.sim1) # osfrv
 
 # A2-Q8
 
-ts.sim9 <- arima.sim(n=250, list(ar=c(0.7), ma=c(0.3))) # HVERNIG Á AÐ SETJA HELVÍTIS D FAKTORINN INN OG HVAÐ ER HANN???
+ts.sim9 <- arima.sim(list(order = c(1,1,1), ar = 0.7, ma=0.3), n = 250)
 
 
 ts.sim9_diff1 <- diff(ts.sim9, differences=1) #first difference
-plot.ts(ts.sim9_diff1)
-lines(ts.sim9)
+plot.ts(ts.sim9_diff1, col=3)
+lines(ts.sim9, col=2)
+
