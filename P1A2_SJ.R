21fd5fe2b1c204dd11f9fa631188caf68dfc7b90
sigurdurjonsson <sigurdurjon15@ru.is>
2016-09-11 14:53:03 +0000

    Update P1A2_SJ.R

--- P1A2_SJ.R
+++ P1A2_SJ.R
@@ -4,24 +4,29 @@ library(RCurl)
 x = getURL("https://raw.githubusercontent.com/juliuspeturg/Assignment1/master/simdata.csv")
 simdata = read.csv(text=x,stringsAsFactors = FALSE,header=TRUE, sep="")
 
-# simdata <- read.csv("simdata.csv", header=TRUE, sep="") <<<< ATH breyta í URL import
-
 # A2-Q1: Plot the data
 
-# á sama grafi
-plot(simdata$y1, type="l",col=2)
-lines(simdata$y3,col=3)
-lines(simdata$y2,col=4)
+attach(simdata)
 
-# # á þremur gröfum
-# attach(simdata)
-# par(mfrow=c(3,1)) 
-# plot(simdata$y1, type="l",col=2)
-# plot(simdata$y2, type="l",col=3)
-# plot(simdata$y3, type="l",col=4)
+# á sama grafi
+plot(y1, type="l",col='red', main="Simdata time series",
+     xlab="Time", ylab="Data")
+lines(y2,col='blue')
+lines(y3,col='green')
+legend('topright', legend=c("y1", "y2","y3"),
+       col=c('red','blue','green'), lty=1, bty='y', cex=.85)
+
+
+# á þremur gröfum
+
+par(mfrow=c(3,1))
+plot(y1, type="l",col=2, main="Time series y1", xlab="Time", ylab="Data")
+plot(y2, type="l",col=3, main="Time series y2", xlab="Time", ylab="Data")
+plot(y3, type="l",col=4, main="Time series y3", xlab="Time", ylab="Data")
+
 
 # # A2-Q2: Plot acf and pacf
-#
+
 attach(simdata)
 par(mfrow=c(3,2)) 
 # par(mfrow=c(1,1)) 
@@ -32,17 +37,13 @@ pacf(y2)
 acf(y3)
 pacf(y3)
 
-
-
-
 # A2-Q3: Identify the models using arima
 
 arima(y1)
-auto.arima(y1)
+arima(y2)
+arima(y3)
 
-
-
-
+auto.arima(y1) # líka hægt að nota auto.arima
 
 require(forecast)
 fit1.arima <- arima(y1, order=c(0,1,0))
@@ -51,25 +52,26 @@ plot(forecast(fit1.arima,12))
 
 # A2-Q4: Simulate a model
 
-ts.sim <- arima.sim(list(order = c(1,0,0), ar = 0.9), n = 250) # ath
+ts.sim <- arima.sim(list(order = c(1,0,0), ar = 0.9), n = 250)
 
 
 # A2-Q5: Plot timeseries, ACF and PACF
 
-par(mfrow=c(3,1)) 
-plot(ts.sim)
-acf(ts.sim)
-pacf(ts.sim)
+layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))  
+plot(ts.sim, main="Simulated time series ts.sim")
+acf(ts.sim, main="ACF for ts.sim")
+pacf(ts.sim, main="PACF for ts.sim")
 
 # A2-Q6: Create own plot function that plots the timeseries, ACF and PACF
 
 acf.plot <- function(y){
-  par(mfrow=c(3,1)) 
-  plot(y)
-  acf(y)
-  pacf(y)
+  layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE)) 
+  plot(y, main=c("Simulated time series",deparse(substitute(y))))
+  acf(y, main=c("ACF for",deparse(substitute(y))))
+  pacf(y,main=c("PACF for",deparse(substitute(y))))
 }
 
+# Plotting the series, ACF and PACF with the new function
 acf.plot(ts.sim)
 
 
@@ -84,8 +86,16 @@ ts.sim6 <- arima.sim(n=250, list(ma=c(0.5,0.5)))
 ts.sim7 <- arima.sim(n=250, list(ma=c(-0.9)))
 ts.sim8 <- arima.sim(n=250, list(ar=c(0.5), ma=c(-0.8)))
 
+# Plotting the simulated series with the acf function
+acf.plot(ts.sim1)
+acf.plot(ts.sim2)
+acf.plot(ts.sim3)
+acf.plot(ts.sim4)
+acf.plot(ts.sim5)
+acf.plot(ts.sim6)
+acf.plot(ts.sim7)
+acf.plot(ts.sim8)
 
-acf.plot(ts.sim1) # osfrv
 
 # A2-Q8
 
